4fe90811ce546f3714947f6f46e4a51b4f0b739b
mr1or@naver.com <wkdgns26@gmail.com>
2020-01-04 23:24:14 +0900

    edit

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,28 @@
-import { Link } from "gatsby"
+  import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Menu from "./nav/menu"
+import styled from "styled-components"
 
+// RGB (16, 210, 73 )
 const Header = ({ siteTitle }) => (
   <header
+    className="header"
     style={{
-      background: `rebeccapurple`,
+      backgroundColor:`RGB(112, 122, 139)`,
       marginBottom: `1.45rem`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+    <div style={{
+      width: `974px`,
+      height: `65px`,
+      margin: `0 auto`
+    }}>
+      <Menu name="Home" dir="/" />
+      <Menu name="skill" dir="/skill" />
+      <Menu name="Portfolio" dir="/portfolio" />
+      <Menu name="History" dir="/history" />
+      <Menu name="Contact" dir="/contact" />
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Hi, I'm Hooni</h1>
+    <p>Full stack Developer</p>
+    
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
